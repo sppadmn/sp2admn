163447be11be126f974500129ccaca20e16adeb2
SinwarPrem <premsinwar4@gmail.com>
2015-11-22 17:02:27 +0530

    frontend progress

--- adduser.php
+++ adduser.php
@@ -31,13 +31,76 @@
 		file_put_contents("users.json", json_encode($json, JSON_PRETTY_PRINT));
 ?>
 <html>
+	<head>
+		<title>CourSea</title>
+		<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+ 		 <link rel="stylesheet" type="text/css" href="style.css">
+  		<link href='http://fonts.googleapis.com/css?family=Lobster' rel='stylesheet' type='text/css'>
+  		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+  		<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+		<style type="text/css">
+		body{
+			font-size: 20px;
+			background-image: url("Untitled-1.jpg");
+			background-repeat: no-repeat;
+		}
+		fieldset{
+			margin-top: 6%;
+			width: 25%;	
+			border: 5px SkyBlue;	
+			background-color: rgba(255, 153, 204,0.5);
+			border-radius: 50px 5px;
+			width: 425px;
+			height: auto;
+			padding: 50px;
+			font-size: 25px;
+			box-shadow: 10px 10px 5px DarkSlateGray;
+		}
+		.button {
+    		background-color: SkyBlue;
+    		border: none;
+    		padding: 15px 32px;
+    		text-align: center;
+    		text-decoration: none;
+    		font-size: 16px;
+    		cursor: pointer;
+    		border-radius: 10px;
+    		font-size: 20px;
+		}
+		.button:hover {
+    		box-shadow: 0 12px 16px 0 rgba(0,0,0,0.24),0 17px 50px 0 rgba(0,0,0,0.19);
+    		color: white;
+		}
+		.height{
+			height: 40px;
+			border-radius: 5px;
+			border: none;
+			padding: 20px;
+			font-size: 18px;
+		}
+
+</style>
+	</head>
 	<body>
-		<form method="post" >
-			Username:<input type="text" name="user"><br>
-			Admission No.:<input type="text" name="admin"><br>
-			Passwod:<input type="password" name="pass">
-			<input type="submit">
-		</form>
+		<div class="container">
+			<div class="col-md-4"></div>
+			<div class="col-md-4">
+				<img src="images/logo.png" height="100px" width="400px" style="margin-top:2%;">
+			</div>
+			<div class="col-md-4"></div>
+		</div>
+		<form method="post">
+		<center><fieldset>
+			<input type="text" name="full name" placeholder="Enter Your Name" size="30" class="height" name="user">
+			<br><br>
+			<input type="text" placeholder="Enter Your Admission No."  size="30" class="height" name="admin">
+			<br><br>	
+			<input type="password" name="password" placeholder="Enter Your Password"  size="30" class="height" name="pass">
+			<br><br>
+			<center><button type="submit" class="button">Add User</button>
+		</fieldset></center>
+	</form>
+
 
 	</body>
 </html>
--- login.php
+++ login.php
@@ -6,14 +6,17 @@
 <html>
 <head>
 	<title>LOGIn</title>
+	 <meta charset="utf-8">
+    <link rel="icon" type="image/jpg" sizes="16x16" href="publi/logo_1.jpg">
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link href='http://fonts.googleapis.com/css?family=Lobster' rel='stylesheet' type='text/css'>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
 </head>
 <body>
-	<form method="post">
-		user:<input type="text" name="username"><br>
-		pass:<input type="password" name="password"><br>
-		<input type="submit">
-	</form>
 	<?php
+		$error="";
 	   //variables
 		$user=$pass="";
 		//array having all user information
@@ -27,7 +30,7 @@
 
 		//check for users
 		if(empty($user) or empty($pass)){
-			echo "<h4>Please fill the field</h4>";
+			$_SESSION["error"]="Please fill the form";
 		}
 		else if ($user=="admin" and $pass="123") {
 			# code...
@@ -39,17 +42,48 @@
 				header("location:ind.php");
 			}
 			else{
-				echo "Wrong username password combination";
+				$_SESSION["error"]="Wrong username password combination";
 			}
 		}
 		else{
-			echo "Username is not in databasee";
+			$_SESSION["error"]="Username is not in database";
 		}
 
 		$_SESSION['username']=$user;//session variables
 		
 			
 	?>
+
+	<div class="container-fluid">
+    <img src="images/logo.png" >
+ 		<!--LOGIN FORM -->
+
+ 		<div class="row">
+ 			
+ 			<div class="col-md-4  loginform">
+ 				<h4><?php echo $_SESSION["error"];?></h4>
+ 				<form role="form" method="post">
+
+				  	<div class="form-group">
+    			  		<label class="sr-only control-label" for="email">User ID:</label>
+    			  		<input type="text" class="form-control input-lg" id="email" placeholder="Enter Username here" size="20px" name="username">
+  					</div>
+  					<div class="form-group">
+    					<label class="sr-only" for="pwd">Password:</label>
+    					<input type="password" class="form-control input-lg" id="pwd" placeholder="Enter Password here" name="password">
+  					</div>
+  					<div class="checkbox">
+    					<label><input type="checkbox">      Remember me</label>
+  					</div>
+  					<button type="submit" class="btn btn-default btn-block btn-info">Login</button>
+     
+				</form>
+      </div>
+      <div class="col-md-8"></div>
+ 			</div>
+ 		</div>
+ 	</div>
+	
 	
 	
 	
